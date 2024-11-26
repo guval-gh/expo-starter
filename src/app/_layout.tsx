08c254f8fab787ba0430b22cbc9488b3dd8963b4
guval <177042063+guval-gh@users.noreply.github.com>
2024-11-26 21:27:20 +0100

    🚨 Apply sort imports config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,12 @@
+import { useEffect } from 'react'
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
-import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
+
+import { useFonts } from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
-import { useEffect } from 'react'
+
 import 'react-native-reanimated'
 
 import { useColorScheme } from '@/hooks/useColorScheme'
--- src/components/ui/TabBarBackground.ios.tsx
+++ src/components/ui/TabBarBackground.ios.tsx
@@ -1,6 +1,9 @@
-import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
-import { BlurView } from 'expo-blur'
 import { StyleSheet } from 'react-native'
+
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
+
+import { BlurView } from 'expo-blur'
+
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export default function BlurTabBarBackground() {
